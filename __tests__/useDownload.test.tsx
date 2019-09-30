9d292188cbcfa3f506c3df9bdc20cb3e90b38a58
Jinke.Li <1359518268@qq.com>
2019-09-30 10:17:56 +0800

    feat(use-download): 增加 useDownload 测试

---
+++ __tests__/useDownload.test.tsx
@@ -0,0 +1,22 @@
+import { useDownload } from '../src'
+import { renderHook } from '@testing-library/react-hooks'
+
+describe('useDownload', () => {
+  let download
+  beforeAll(() => {
+    download = renderHook(() => useDownload())
+  })
+  it('should be defined', () => {
+    expect(download).toBeDefined()
+  })
+  it('should return a download instance function', () => {
+    const instance = download.result.current
+    expect(instance).toBeInstanceOf(Function)
+  })
+  it('should create a new download instance', () => {
+    const createInstance = download.result.current
+    const instance = createInstance({ name: '1', data: '1' })
+    expect(instance).toHaveProperty('download')
+    expect(instance.download).toBeInstanceOf(Function)
+  })
+})
